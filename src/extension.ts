ce6af4d7338cf4bd305f026860e6c10d35959b5f
www-tacos-asrock <www.0923.tacos@gmail.com>
2021-12-10 17:35:39 +0900

    起動時にサーバを起動させるための変更
    テンプレートに言語サーバを起動させる一連の処理を新規に追加

--- src/extension.ts
+++ src/extension.ts
@@ -2,10 +2,16 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+// 追加import
+import * as path from "path";
+//@ts-ignore
+import { Executable, LanguageClient, LanguageClientOptions, StreamInfo, ServerOptions } from 'vscode-languageclient';
+
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "lse-in-python" is now active!');
@@ -20,6 +26,42 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+  // ここまでテンプレートのまま
+  // ここから追加分
+
+  // サーバ側のエントリーポイントファイルの絶対パスを取得する
+  const serverPath: string = context.asAbsolutePath(
+    path.join("server", "server.py")
+  );
+  // サーバ側の設定を定義する
+  const serverOptions: ServerOptions = {
+    command: "python",
+    args: [serverPath],
+  };
+
+  // クライアント側の設定を定義する
+  const clientOptions: LanguageClientOptions = {
+    // プレーンテキストファイルとPythonファイルに対して有効になる
+    documentSelector: [
+      {
+        scheme: "file",
+        language: "plaintext"
+      },
+      {
+        scheme: "file",
+        language: "python"
+      }
+    ]
+  };
+  // クライアントを起動する
+  const client: LanguageClient = new LanguageClient(
+    "lseInPython",
+    "Language Server Extension in Python",
+    serverOptions,
+    clientOptions
+  );
+  client.start();
 }
 
 // this method is called when your extension is deactivated
