da86833f722c0c720f6865dc2f038424bccc836d
www-tacos-asrock <www.0923.tacos@gmail.com>
2021-12-22 09:08:43 +0900

    記事に合わせてコードを整理
    テンプレートに最初から入っていたコードの削除・コメント文の軽微な修正・使うつもりはないけど一応deactivateの追加

--- src/extension.ts
+++ src/extension.ts
@@ -2,38 +2,21 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// 追加import
+
 import * as path from "path";
 //@ts-ignore
 import { Executable, LanguageClient, LanguageClientOptions, StreamInfo, ServerOptions } from 'vscode-languageclient';
 
+let client: LanguageClient;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "lse-in-python" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('lse-in-python.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from LSE in Python!');
-	});
-
-	context.subscriptions.push(disposable);
-
-  // ここまでテンプレートのまま
-  // ここから追加分
-
   // サーバ側のエントリーポイントファイルの絶対パスを取得する
   const serverPath: string = context.asAbsolutePath(
     path.join("server", "server.py")
   );
+
   // サーバ側の設定を定義する
   const serverOptions: ServerOptions = {
     command: "python",
@@ -42,7 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
 
   // クライアント側の設定を定義する
   const clientOptions: LanguageClientOptions = {
-    // プレーンテキストファイルとPythonファイルに対して有効になる
+    // プレーンテキストファイルとPythonファイルに対して有効にする
     documentSelector: [
       {
         scheme: "file",
@@ -54,8 +37,9 @@ export function activate(context: vscode.ExtensionContext) {
       }
     ]
   };
+
   // クライアントを起動する
-  const client: LanguageClient = new LanguageClient(
+  client = new LanguageClient(
     "lseInPython",
     "Language Server Extension in Python",
     serverOptions,
@@ -65,4 +49,10 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  if(!client) {
+    return undefined;
+  } else {
+    return client.stop();
+  }
+}
